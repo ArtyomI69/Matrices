cd5ea14518516d29ceffd28d89fa794b03feedc8
Artyom Ivanov <Artyom655@yandex.ru>
2022-10-16 00:53:28 +0300

    добавил регионы и коментарии в классе СЛАУ

--- Matrixes/SoLE.cs
+++ Matrixes/SoLE.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibraryMatrices {
     public class SoLE {
+        #region Поля
         /// <summary>
         /// Коэффициенты линейных уравнений
         /// </summary>
@@ -20,14 +21,29 @@ namespace ClassLibraryMatrices {
         /// Свободные коэффициенты линейных уравнений
         /// </summary>
         public Matrix B;
+        #endregion
 
+        #region Конструктор
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="a">матрица А, предсавляющая коэффициенты</param>
+        /// <param name="b">матрица B, предсавляющая свободные коэффициенты</param>
+        /// <exception cref="Exception">если матрица А не квадратная или кол-во свободных элементов меньше чем кол-во уравнений</exception>
         public SoLE(Matrix a, Matrix b) {
             if (!a.IsSquare() || a.M != b.M) throw new Exception();
 
             A = a;
             B = b;
         }
+        #endregion
 
+        #region Public методы
+        /// <summary>
+        /// Решение системы линейных алгебраических уравнений
+        /// </summary>
+        /// <returns>лист элементов, представляющих ответы</returns>
+        /// <exception cref="Exception">если невозможно решить СЛАУ</exception>
         public List<double> Solution() {
             List<double> result = new List<double>();
             double mainDeterminant = A.Determinant();
@@ -44,6 +60,10 @@ namespace ClassLibraryMatrices {
             return result;
         }
 
+        /// <summary>
+        /// Конвертация системы линейных алгебраических уравнений в строку
+        /// </summary>
+        /// <returns>строка</returns>
         public override string ToString() {
             string s = "";
             for (int j = 0; j < N; j++) {
@@ -81,7 +101,14 @@ namespace ClassLibraryMatrices {
             }
             return s;
         }
+        #endregion
 
+        #region Private методы
+        /// <summary>
+        /// Проверяет если все элементы в ряду равны 0
+        /// </summary>
+        /// <param name="row">строка</param>
+        /// <returns>True - если все элементы в ряду равны 0, False - хотябы 1 элемент не равен 0</returns>
         private bool AreAllCoefInRowEqualZero(int row) {
             for (int i = 0; i < N; i++) {
                 if (A[i, row] != 0) return false;
@@ -89,6 +116,12 @@ namespace ClassLibraryMatrices {
             return true;
         }
 
+        /// <summary>
+        /// Проверяет если элементы до определённой позиции в ряду равны 0
+        /// </summary>
+        /// <param name="row">строка</param>
+        /// <param name="column">столбец</param>
+        /// <returns>True - если все элементы до данной позиции равны 0, False - хотябы 1 элемент не равен 0</returns>
         private bool AreCoefBeforeEqualZero(int row, int column) {
             for (int i = 0; i < column; i++) {
                 if (A[i, row] != 0) return false;
@@ -96,6 +129,10 @@ namespace ClassLibraryMatrices {
             return true;
         }
 
+        /// <summary>
+        /// Вычисление определителей
+        /// </summary>
+        /// <returns>массив определителей</returns>
         private double[] CalculateDeterminants() {
             double[] determinants = new double[N];
             for (int j = 0; j < N; j++) {
@@ -108,11 +145,17 @@ namespace ClassLibraryMatrices {
             return determinants;
         }
 
+        /// <summary>
+        /// Проверяет если все определители равны 0
+        /// </summary>
+        /// <param name="determinants">массив определителей</param>
+        /// <returns>True - если все элементы в ряду равны 0, False - хотябы 1 элемент не равен 0</returns>
         private bool AreDeterminantsEqualZero(double[] determinants) {
             foreach (double determinant in determinants) {
                 if (determinant != 0) return false;
             }
             return true;
         }
+        #endregion
     }
 }
